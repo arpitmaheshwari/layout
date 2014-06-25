b5f4ae86028accfc2579a4b9199a0386b2f2ad52
unknown <amaheshwari@AMAHESHWARI2L.ptcnet.ptc.com>
2014-06-25 17:12:24 +0530

    Adding lego hours presentation

---
+++ Login Page Changes.mm
@@ -0,0 +1,50 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1401965717591" ID="ID_1337815933" MODIFIED="1402035091058" TEXT="Current Page">
+<node CREATED="1401965773409" FOLDED="true" HGAP="80" ID="ID_523236884" MODIFIED="1402035488341" POSITION="right" TEXT="Dual Role" VSHIFT="-7">
+<node CREATED="1401965813800" ID="ID_1171456650" MODIFIED="1402035139288" TEXT="Welcome Screen (Right Half)">
+<node CREATED="1401965876585" FOLDED="true" ID="ID_619228062" MODIFIED="1402035475381" TEXT="Do we really need it ?">
+<node CREATED="1401965897770" HGAP="49" ID="ID_1058974842" MODIFIED="1402035453794" VSHIFT="-35">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="how%20we%20read%20web.PNG" style="width: 100px" />
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1401965921986" ID="ID_49591128" MODIFIED="1401968492596" TEXT="We already send a welcome email to users introducing us."/>
+<node CREATED="1401965957725" ID="ID_787525551" MODIFIED="1401967293296" TEXT="We already have an about us link on page."/>
+</node>
+</node>
+<node CREATED="1401965825692" ID="ID_352788096" MODIFIED="1402035476951" TEXT="Login screen (Left half)">
+<node CREATED="1401967197135" ID="ID_256699482" MODIFIED="1401967202668" TEXT="WBT Login"/>
+<node CREATED="1401967203206" ID="ID_1629236218" MODIFIED="1401967208995" TEXT="ILT Login"/>
+</node>
+</node>
+<node CREATED="1401967334821" FOLDED="true" ID="ID_549328661" MODIFIED="1402035506438" POSITION="left" TEXT="Improvements required">
+<node CREATED="1401967363984" ID="ID_230922764" MODIFIED="1401967390539" TEXT="Too much content (Lot of clutter)"/>
+<node CREATED="1401967393061" ID="ID_1647969881" MODIFIED="1401967425398" TEXT="Not optimized for smaller screen sizes"/>
+<node CREATED="1401967426024" ID="ID_640149045" MODIFIED="1401967512689" TEXT="Color scheme"/>
+<node CREATED="1401967514044" ID="ID_238444150" MODIFIED="1401967538055" TEXT="Toggling between login forms not very intuitive"/>
+<node CREATED="1401967640923" ID="ID_504955398" MODIFIED="1401967662790" TEXT="No client side validations"/>
+</node>
+<node CREATED="1401967554999" FOLDED="true" HGAP="-24" ID="ID_1501239304" MODIFIED="1402037255625" POSITION="right" TEXT="Changes made" VSHIFT="130">
+<node CREATED="1401967560865" ID="ID_642472679" MODIFIED="1401967594407" TEXT="Reduction of content"/>
+<node CREATED="1401967595066" ID="ID_42182611" MODIFIED="1401967616371" TEXT="Mobile first"/>
+<node CREATED="1402036907381" ID="ID_1864499060" MODIFIED="1402036976528" TEXT="Larger sized buttons for smartphone users (who use thumbs to click)"/>
+<node CREATED="1401967822083" ID="ID_1758761665" MODIFIED="1401967846085" TEXT="Different style for header &amp; footer"/>
+<node CREATED="1401967616958" ID="ID_862405658" MODIFIED="1401967622154" TEXT="Use of web fonts"/>
+<node CREATED="1401967623174" ID="ID_38318740" MODIFIED="1401967632226" TEXT="JS Validations"/>
+<node CREATED="1401967686069" ID="ID_1382871996" MODIFIED="1401968377953" TEXT="Consistent with Flat UI "/>
+<node CREATED="1401967783752" ID="ID_1089680983" MODIFIED="1401967791741" TEXT="CAPS lock on warning"/>
+<node CREATED="1401967919926" ID="ID_1675388523" MODIFIED="1401968248496" TEXT="Language menu shows languages in cooresponding script"/>
+<node CREATED="1402036825568" ID="ID_388836078" MODIFIED="1402036859272" TEXT="No remember my username (modern browsers do that for you)"/>
+<node CREATED="1402036872547" ID="ID_526753093" MODIFIED="1402036889274" TEXT="Single link for forgot password"/>
+</node>
+</node>
+</map>
